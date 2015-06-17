54af371ce1c8dbcce4851183856c127d01fd1c46
kristiansl <kristianmeier@kristians-MacBook-Air.local>
2015-06-16 18:34:57 -0700

    here we go

--- src/test/java/com/saucelabs/SampleSauceTest.java
+++ src/test/java/com/saucelabs/SampleSauceTest.java
@@ -178,7 +178,6 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
         driver.findElement(By.id("sopq-hotel-dest")).click();
         driver.findElement(By.id("sopq-hotel-dest")).clear();
         driver.findElement(By.id("sopq-hotel-dest")).sendKeys("Las Vegas, NV ");
-        //Thread.sleep(7000);
         driver.findElement(By.id("hotel-btn-submit-sopq")).click();
     }
  
